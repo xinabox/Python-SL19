741ed761e3b6b900e7900fc724079a6a72148efa
luqmaanb <lb@xinabox.cc>
2020-06-17 16:52:51 +0200

    Updat version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xinabox-SL19",
-    version="0.0.3",
+    version="0.0.4",
     author="Luqmaan Baboo",
     author_email="luqmaanbaboo@gmail.com",
     description="Infrared object temperature sensor",
